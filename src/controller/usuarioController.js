e1ad4a868647d9613fc63acd81115a3ca7578215
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-06-10 19:56:26 -0300

    chore: finalização da rota usuario(get, post, put, delete) e validação
    obs: fazer uma rota get pra filtro futuramente

--- src/controller/usuarioController.js
+++ src/controller/usuarioController.js
@@ -6,18 +6,93 @@ class UsuarioController {
   static async listarUsuario(req, res) {
     const listausuarios = await prisma.usuario.findMany();
 
-    res.status(200).json(listausuarios);
+    if (listausuarios.length > 0) {
+      res.status(200).json(listausuarios);
+    } else {
+      res.status(200).json("Nenhum usuario na base de dados");
+    }
   }
 
   static async insertUsuario(req, res) {
-    await prisma.usuario.create({
-      data: {
-        nome: req.body.nome,
-        email: req.body.email
-      }
-    })
+    try {
+      await prisma.usuario.create({
+        data: {
+          nome: req.body.nome,
+          email: req.body.email,
+        },
+      });
 
-    res.status(201).json(`Usuario ${req.body.nome} foi cadastrado com sucesso`);
+      res
+        .status(201)
+        .json(`O usuario ${req.body.nome} foi cadastrado com sucesso`);
+    } catch (erro) {
+      if (erro.code === "P2002" && erro.meta?.target.includes("email")) {
+        res
+          .status(400)
+          .json(
+            `Ocorreu um erro ao cadastrar o usuário: Este endereço de e-mail já está em uso.`
+          );
+      } else {
+        res
+          .status(500)
+          .json(`Ocorreu um erro ao cadastrar o usuário: ${erro.message}`);
+      }
+    }
+  }
+
+  static async updateUsuario(req, res) {
+    try {
+      await prisma.usuario.update({
+        where: {
+          id: req.params.id,
+        },
+        data: {
+          nome: req.body.nome,
+          email: req.body.email,
+        },
+      });
+
+      res.status(201).json(`O usuario foi atualizado com sucesso`);
+    } catch (erro) {
+      if (erro.code === "P2002" && erro.meta?.target.includes("email")) {
+        res
+          .status(400)
+          .json(
+            `Ocorreu um erro ao atualizar o usuário: Este endereço de e-mail já está em uso.`
+          );
+      } else {
+        res
+          .status(500)
+          .json(`Ocorreu um erro ao atualizar o usuário: ${erro.message}`);
+      }
+    }
+  }
+
+  static async deleteUsuario(req, res) {
+    try {
+      await prisma.usuario.delete({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      res.status(200).json(`O usuario foi deletado com sucesso`);
+    } catch (erro) {
+      if (
+        erro.code === "P2025" &&
+        erro.meta?.relationName === "PostagemToUsuario"
+      ) {
+        res
+          .status(400)
+          .json(
+            "Ocorreu um erro ao excluir o usuário: Existem postagens associadas a este usuário. Por favor, exclua as postagens primeiro."
+          );
+      } else {
+        res
+          .status(500)
+          .json(`Ocorreu um erro ao excluir o usuário: ${erro.message}`);
+      }
+    }
   }
 }
 
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 
 routes.get("/usuario", UsuarioController.listarUsuario);
 routes.post("/usuario", UsuarioController.insertUsuario);
+routes.put("/usuario/:id", UsuarioController.updateUsuario);
+routes.delete("/usuario/:id", UsuarioController.deleteUsuario);
 
 export default routes;
