aaeaf8550d13561a6f027e83533dbfb4eac14344
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-06-10 16:55:52 -0300

    chore finalização do schema

--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import UserController from "../controller/UserController.js";
-
-const routes = express.Router();
-
-routes.get("/users", UserController.listarUsers);
-
-export default routes;
