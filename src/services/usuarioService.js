859b5a93d39d21e023fa626745cb0fbf6d4108fd
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-06-12 13:46:35 -0300

    chore: criação dos diretorios services e prisma

---
+++ src/services/usuarioService.js
@@ -0,0 +1,31 @@
+import prisma from "../prisma/client.js";
+
+export const insertUsuario = async (data) => {
+  return await prisma.usuario.create({
+    data,
+  });
+};
+
+export const listarUsuario = async () => {
+  return await prisma.usuario.findMany();
+};
+
+export const updateUsuario = async (id, data) => {
+  return await prisma.usuario.update({
+    where: {
+      id: id,
+    },
+    data: {
+      nome: data.nome,
+      email: data.email,
+    },
+  });
+};
+
+export const deleteUsuario = async (id) => {
+  return await prisma.usuario.delete({
+    where: {
+      id: id,
+    },
+  });
+};
