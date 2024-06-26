a789c19c69e9aa9d2cbf89076964e7155fb61364
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-06-26 15:57:48 -0300

    chore: fazer referencia as rotas Comentario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import usuario from "./usuarioRoutes.js";
-import postagem from "./postagemRoutes.js"
+import postagem from "./postagemRoutes.js";
+import comentario from "./comentarioRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => res.status(200).send("Api Postagens"));
-  app.use(express.json(), usuario, postagem);
+  app.use(express.json(), usuario, postagem, comentario);
 };
 
 export default routes;
