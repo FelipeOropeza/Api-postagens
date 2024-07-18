c654f6d542e57d5abbd6e62d9ebdeb55398ee654
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-07-18 00:32:18 -0300

    feat: configuração do jeito certo

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-import cors from "cors";
 import app from "./src/app.js";
 import "dotenv/config";
-
-app.use(cors());
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor ok");
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+import cors from "cors";
 import express from "express";
 import routes from "./routes/index.js";
 
 const app = express();
+app.use(cors());
 routes(app);
 
 export default app;
