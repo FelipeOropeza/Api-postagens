09fe2fcecd267725da7ebfef43109ec49ad9a09c
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-07-18 00:20:00 -0300

    feat: alteração

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import cors from "cors";
 import app from "./src/app.js";
 import "dotenv/config";
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor ok");
