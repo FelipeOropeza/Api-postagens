7580789ec65155702a934fea037048f51d48c371
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-07-18 00:12:43 -0300

    feat: dá a permissão para a aplicação roda em outro serviço

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-// import http from "http";
+import cors from "cors";
 import app from "./src/app.js";
 import "dotenv/config";
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor ok");
