f5767707c3f01ddd6dda1d3684e95615c73202f0
FelipeOropeza <felipe2006.co@gmail.com>
2024-10-19 20:39:20 -0300

    feat: cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ import express from "express";
 import routes from "./routes/index.js";
 
 const app = express();
-app.use(cors({
-    origin: 'https://dica-comunity-dtf1qeogs-felipeoropezas-projects.vercel.app',
-  }));
+app.use(cors());
 routes(app);
 
 export default app;
